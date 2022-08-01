bafc7c55be34e77b4362d385d60001d8fc242d2f
igoradigey01 <ett66@mail.ru>
2022-08-01 19:10:55 +0300

    feat(xl01):add

---
+++ apps/xl01/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'x01-v1-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'xl01';
+}
