b96e4c6477afee15de2e27e2a6953dfc3c449271
igoradigey01 <ett66@mail.ru>
2022-08-08 21:11:22 +0300

    feat(stared-style*lib):create

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import {MaterialModule  } from './material.module'
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MaterialModule    
+  ],
   declarations: [HeaderComponent],
   exports:[HeaderComponent]
 })
