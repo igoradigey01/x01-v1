031bb9f61eba140d31e6ee1a223b9d7665a69395
igoradigey01 <ett66@mail.ru>
2022-08-10 07:05:40 +0300

    chore(shared-style):change-location

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
-import {MaterialModule  } from './material.module'
+import {MaterialModule  } from './material.module';
+//import {SharedStylesModule} from '@x01-v1/shared-styles'
 
 @NgModule({
   imports: [
     CommonModule,
-    MaterialModule    
+    MaterialModule,
+  //  SharedStylesModule    
   ],
   declarations: [HeaderComponent],
   exports:[HeaderComponent]
